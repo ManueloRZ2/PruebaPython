6cc95f2e04ed1b913475db994e2edf236399af37
ManueloRZ2 <nuelarbelaez@gmail.com>
2020-08-29 16:10:13 -0500

    Funciones agregadas
    funciones agregadas y código simplificado

--- PruebaPolinomios.py
+++ PruebaPolinomios.py
@@ -25,7 +25,7 @@ class polinomio():
 	
 			while True:
 				try:
-					coeficientes=int(input(f"Introduzca el coeficiente del término {i+1}: "))
+					coeficientes=float(input(f"Introduzca el coeficiente del término {i+1}: "))
 					self.listaCoef.append(coeficientes)
 					break;
 				except ValueError:
@@ -33,13 +33,13 @@ class polinomio():
 		self.listaVect=np.array(self.listaCoef)
 		print(self.listaVect)			
 
-
+########################## FUNCIONES ################################################
 
 def multipliEscalar(poli):
 
 	while True:
 		try:
-			escalar=int(input("Ingrese el escalar por el que desea multiplicar: "))
+			escalar=float(input("Ingrese el escalar por el que desea multiplicar: "))
 			break;
 		except ValueError:
 			print("Valores no válidos. Recuerde que debe ingresar un valor numerico valido.")
@@ -48,11 +48,65 @@ def multipliEscalar(poli):
 
 
 def sumaPolin(poli1,poli2):
-	print("El resultado de la suma de los polinomios es :",poli1.listaVect+poli2.listaVect)
+	
+	difOrden=(len(poli1.listaVect)-len(poli2.listaVect))
+	vectCeros=np.zeros(abs(difOrden))
+		
+	if len(poli1.listaVect)>len(poli2.listaVect):
+
+		print("El resultado de la suma de los polinomios es :",poli1.listaVect+np.array(vectCeros.tolist()+poli2.listaCoef))
+
+	elif len(poli1.listaVect)<len(poli2.listaVect):
+
+		print("El resultado de la suma de los polinomios es :",poli2.listaVect+np.array(vectCeros.tolist()+poli1.listaCoef))
+
+	else:
+
+		print("El resultado de la suma de los polinomios es :",poli2.listaVect+poli1.listaVect)
+
+
+def restaPolin(poli1,poli2):
+	
+	difOrden=(len(poli1.listaVect)-len(poli2.listaVect))
+	vectCeros=np.zeros(abs(difOrden))
+		
+	if len(poli1.listaVect)>len(poli2.listaVect):
+
+		print("El resultado de la resta de los polinomios es :",poli1.listaVect-np.array(vectCeros.tolist()+poli2.listaCoef))
+
+	elif len(poli1.listaVect)<len(poli2.listaVect):
+
+		print("El resultado de la resta de los polinomios es :",np.array(vectCeros.tolist()+poli1.listaCoef)-poli2.listaVect)
+
+	else:
+
+		print("El resultado de la resta de los polinomios es :",poli1.listaVect-poli2.listaVect)
+
+
+def multipliPolin(poli1,poli2):
+
+	vectResult=np.zeros((len(poli1.listaVect)+len(poli2.listaVect))-1)
+
+	for i in range (len(poli1.listaVect)):
+
+		for j in range(len(poli2.listaVect)):
+				vectResult[i+j]=vectResult[i+j]+(poli1.listaVect[i]*poli2.listaVect[j])
+
+
+	print("El producto de polinomios es: ", vectResult)
+
+
+
 
 
 ################################# Main ###########################################
 
+
+
 polinomio1=polinomio()
-multipliEscalar(polinomio1)
-sumaPolin(polinomio1,polinomio1)
+polinomio2=polinomio()
+
+#multipliEscalar(polinomio1)
+#sumaPolin(polinomio1,polinomio2)
+#restaPolin(polinomio1,polinomio2)
+multipliPolin(polinomio1,polinomio2)
