faac1d0b44417bb4b75c0d76cecd9852917394b3
ManueloRZ2 <nuelarbelaez@gmail.com>
2020-08-30 12:24:56 -0500

    Función para evaluación opcional y comentado
    La función permite evaluar despues de la operación o hacerlo luego con la selección en el menú principal.

--- PruebaAjedrez.py
+++ PruebaAjedrez.py
@@ -3,7 +3,7 @@ import random
 
 class Ficha():
 
-	def __init__(self):
+	def __init__(self):                         # constructor generador del tablero y las posiciones aleatorias de la ficha
 
 		self.posix=(random.randint(0,7))
 		self.posiy=(random.randint(0,7))
@@ -24,88 +24,92 @@ class Ficha():
 class FichaPeon(Ficha):
 	
 	def __init__(self):
-		super().__init__()
-		self.Movimientos()
+		super().__init__()				# llamado del constructor padre
+		self.Movimientos()				# uso de la funcón para determinar movimientos
 	
 	def Movimientos(self):
-		contador=-1
+		contador=-1						# el contador hace referencia a la fila que se está recorriendo
 		for i in self.matrix:									
 			contador+=1									
-													
-			for j in range(len(i)):						
-				if (contador<self.posiy and j==self.posix):		
-					self.matrix[contador][j] = 1
+													                                                 
+			for j in range(len(i)):												# posicionamientos posibles del peón
+				if self.posiy == 6:					
+					if (contador<self.posiy and j==self.posix and 0<self.posiy-contador<3):		# en caso de encontrarse en la posición de inicio del juego
+						self.matrix[contador][j] = 1
+				else:
+					if (contador<self.posiy and j==self.posix and 0<self.posiy-contador<2):		# en cualquier casilla distinta a las de inicio
+						self.matrix[contador][j] = 1
 
-		print("matrix:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
-	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7])			
+		print("Tablero para Peón:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
+	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7], "\n")			
 
 
 ###################################
 class FichaTorre(Ficha):
 	
 	def __init__(self):
-		super().__init__()
-		self.Movimientos()
+		super().__init__()				# llamado del constructor padre
+		self.Movimientos()				# uso de la funcón para determinar movimientos
 
 	def Movimientos(self):
-		contador=-1
+		contador=-1						# el contador hace referencia a la fila que se está recorriendo
 		for i in range(8):									
 			contador+=1									
 													
 			for j in range(8):
-				if ((contador<self.posiy or contador>self.posiy) and j==self.posix):		
+				if ((contador<self.posiy or contador>self.posiy) and j==self.posix):	# posiciona unos cuando la fila y la columna coinciden	
 					self.matrix[contador][j] = 1				
 				elif (contador==self.posiy and j!=self.posix):	
 				    self.matrix[contador][j] = 1
 
-		print("matrix:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
-	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7])	
+		print("Tablero para Torre:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
+	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7], "\n")	
 
 ###################################
 class FichaAlfil(Ficha):
 	
 	def __init__(self):
-		super().__init__()
-		self.Movimientos()
+		super().__init__()				# llamado del constructor padre
+		self.Movimientos()				# uso de la funcón para determinar movimientos
 
 	def Movimientos(self):
-		contador=0										
-		potencia=0
-		contador=self.posiy
+		contador=0						# el contador hace referencia a la fila que se está recorriendo					
+		potencia=0						# para éste caso se recorren las filas desde la posición de
+		contador=self.posiy  			# la ficha hacia arriba, evitando recorrer toda la matríz
 
 		for i in range(6):									
 															
 			contador-=1										
-			potencia+=1
-			if (self.posix-(i+1)>-1 and contador>-1):              # unos diagonal superior izquierda
+			potencia+=1								# condiciones de posicionamiento de los unos, evitando desbordar el tablero
+			if (self.posix-(i+1)>-1 and contador>-1):              		# unos diagonal superior izquierda
 				self.matrix[contador][self.posix-(i+1)]=1
 
-			if (self.posix-(i+1)>-1 and contador+(2*potencia)<8):  # unos diagonal inferior izquierda
+			if (self.posix-(i+1)>-1 and contador+(2*potencia)<8):  		# unos diagonal inferior izquierda
 				self.matrix[contador+(2*potencia)][self.posix-(i+1)]=1
 
-			if (self.posix+(i+1)<8 and contador>-1):               # unos diagonal superior derecha
+			if (self.posix+(i+1)<8 and contador>-1):               		# unos diagonal superior derecha
 				self.matrix[contador][self.posix+(i+1)]=1
 
-			if (self.posix+(i+1)<8 and contador+(2*potencia)<8):   # unos diagonal inferior inferior
+			if (self.posix+(i+1)<8 and contador+(2*potencia)<8):   		# unos diagonal inferior inferior
 				self.matrix[contador+(2*potencia)][self.posix+(i+1)]=1
 
-		print("matrix:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
-	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7])	
+		print("Tablero para Alfil:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
+	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7], "\n")	
 		
 #####################################
 class FichaCaballo(Ficha):
 
 	def __init__(self):
-		super().__init__()
-		self.Movimientos()
+		super().__init__()				# llamado del constructor padre
+		self.Movimientos()				# uso de la funcón para determinar movimientos
 
 	def Movimientos(self):
-		contador=(self.posiy)											
+		contador=(self.posiy)			# el contador hace referencia a la fila que se está recorriendo								
 		for i in range(2):										
 			contador-=1											
 
-			if (self.posix-1>-1 and contador>0 and i==0):
-				self.matrix[contador-1][self.posix-1]=1
+			if (self.posix-1>-1 and contador>0 and i==0):		# posicionamientos de cuatro unos referenciados entre sí
+				self.matrix[contador-1][self.posix-1]=1			# junto con la validación de desborde de tablero
 
 			if (self.posix-1>-1 and contador+3<8 and i==0):
 				self.matrix[contador+3][self.posix-1]=1
@@ -118,8 +122,8 @@ class FichaCaballo(Ficha):
 
 	##########  MOVIMIENTOS MAS INTERNOS HORIZONTALMENTE #########
 
-			if (self.posix-2>-1 and contador>-1 and i==0):
-				self.matrix[contador][self.posix-2]=1
+			if (self.posix-2>-1 and contador>-1 and i==0):		# posicionamientos del resto de unos referenciados entre sí
+				self.matrix[contador][self.posix-2]=1			# junto con la validación de desborde de tablero
 
 			if (self.posix-2>-1 and contador+2<7 and i==0):
 				self.matrix[contador+2][self.posix-2]=1
@@ -130,66 +134,66 @@ class FichaCaballo(Ficha):
 			if (self.posix+2<8 and contador+2<8 and i==0):
 				self.matrix[contador+2][self.posix+2]=1
 
-		print("matrix:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
-	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7])
+		print("Tablero para Caballo:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
+	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7], "\n")
 
 ######################################
 class FichaReina(Ficha):
 
 	def __init__(self):
-		super().__init__()
-		self.Movimientos()
+		super().__init__()				# llamado del constructor padre
+		self.Movimientos()				# uso de la funcón para determinar movimientos
 
 	def Movimientos(self):
-		contador=self.posiy
+		contador=self.posiy  			# el contador hace referencia a la fila que se está recorriendo	
 								
 		potencia=0											
 														
 		for i in range(6):									
-															
+															# movimientos similares del alfil
 			contador-=1										
 			potencia+=1
-			if (self.posix-(i+1)>-1 and contador>-1):              # unos diagonal superios izquierda
+			if (self.posix-(i+1)>-1 and contador>-1):             		 # unos diagonal superior izquierda
 				self.matrix[contador][self.posix-(i+1)]=1
 
-			if (self.posix-(i+1)>-1 and contador+(2*potencia)<8):  # unos diagonal inferior izquierda
+			if (self.posix-(i+1)>-1 and contador+(2*potencia)<8):  		 # unos diagonal inferior izquierda
 				self.matrix[contador+(2*potencia)][self.posix-(i+1)]=1
 
-			if (self.posix+(i+1)<8 and contador>-1):               # unos diagonal superior derecha
+			if (self.posix+(i+1)<8 and contador>-1):               		 # unos diagonal superior derecha
 				self.matrix[contador][self.posix+(i+1)]=1
 
-			if (self.posix+(i+1)<8 and contador+(2*potencia)<8):   # unos diagonal inferior inferior
+			if (self.posix+(i+1)<8 and contador+(2*potencia)<8):  		 # unos diagonal inferior inferior
 				self.matrix[contador+(2*potencia)][self.posix+(i+1)]=1
 
 		contador=-1
-		for i in range(8):									
+		for i in range(8):									# movimientos similares de la torre
 			contador+=1									
 														
-			for j in range(8):
+			for j in range(8):							
 				if ((contador<self.posiy or contador>self.posiy) and j==self.posix):		
 					self.matrix[contador][j] = 1				
 				elif (contador==self.posiy and j!=self.posix):	
 				    self.matrix[contador][j] = 1	
 
-		print("matrix:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
-	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7])		    
+		print("Tablero para Reina:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
+	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7],"\n")		    
 
 class FichaRey(Ficha):
 
 	def __init__(self):
-		super().__init__()
-		self.Movimientos()
+		super().__init__()					# llamado del constructor padre
+		self.Movimientos()					# uso de la funcón para determinar movimientos
 
 	def Movimientos(self):
-		contador=self.posiy-1										
+		contador=self.posiy-1						# el contador hace referencia a la fila que se está recorriendo					
 				
-		if (self.posix-1>-1 and contador>-1):             	 	# unos diagonal superios izquierda
+		if (self.posix-1>-1 and contador>-1):             	 		# unos diagonal superior izquierda
 			self.matrix[contador][self.posix-1]=1
 
 		if (self.posix-1>-1 and contador+2<8):  				 	# unos diagonal inferior izquierda
 			self.matrix[contador+2][self.posix-1]=1
 
-		if (self.posix+1<8 and contador>-1):               		# unos diagonal superior derecha
+		if (self.posix+1<8 and contador>-1):               			# unos diagonal superior derecha
 			self.matrix[contador][self.posix+1]=1
 
 		if (self.posix+1<8 and contador+2<8):   					# unos diagonal inferior inferior
@@ -204,15 +208,18 @@ class FichaRey(Ficha):
 		if (self.posiy+1<8):
 			self.matrix[self.posiy+1][self.posix]=1
 
-		print("matrix:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
-	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7])
+		print("Tablero para Rey:\n",self.matrix[0],"\n",self.matrix[1],"\n",self.matrix[2],"\n",self.matrix[3],"\n",self.matrix[4],"\n",
+	  	self.matrix[5],"\n",self.matrix[6],"\n",self.matrix[7], "\n")
 
+
+
+print("El siguiente programa se encarga de mostrar las posiciones de posibles movimientos de las fichas de ajedrez. Las fichas marcan su posición de forma aleatoria con un 8, los movimientos posibles se determinan por 1 y las posiciones inválidas con 0.","\n")
 while True:
 	
 	while True:
 
 		try:
-			fichaSeleccion=input("Ingrese la ficha de la cual desea ver movimientos (peon, torre, alfil, caballo, reina, rey). Para finalizar el programa, ingerese: salir :")
+			fichaSeleccion=input("Para continuar por favor ingrese la ficha de la cual desea ver movimientos (peon, torre, alfil, caballo, reina, rey). Para finalizar el programa, ingerese 'salir' :")
 			break;
 		except ValueError:
 			print("La ficha ingresada no existe, por favor intentelo de nuevo.")
--- PruebaPolinomios.py
+++ PruebaPolinomios.py
@@ -3,13 +3,13 @@ import numpy as np
 class polinomio():
 
 	def __init__(self):
-		self.listaCoef=[]
-		self.listaVect=np.zeros(1)
-		self.inicializar()
+		self.listaCoef=[]				# inicializar los vectores para ingresar los coeficientes
+		self.listaVect=np.zeros(1)		# usando listas para concatenar valores y pasarlos a vector
+		self.inicializar()				# posteriormente
 
 	def inicializar(self):
 		while True:
-
+										# Validación de la entrada propocionada por el usuario
 			try:
 				orden=int(input("Por favor, ingrese el orden del polinomio:"))
 				if orden<0:
@@ -18,11 +18,11 @@ class polinomio():
 					break;
 
 			except ValueError:
-				print("Valores no válidos. Recuerde que debe ingresar un valor numerico valido.")
+				print("Valores no válidos. Recuerde que debe ingresar un valor numérico valido.")
 
 		self.listaCoef=[]
 		for i in range(orden+1):
-	
+										# recolección de los coeficientes del polinomio en cuestión
 			while True:
 				try:
 					coeficientes=float(input(f"Introduzca el coeficiente del término {i+1}: "))
@@ -30,7 +30,7 @@ class polinomio():
 					break;
 				except ValueError:
 					print("Coeficiente inválido. Por favor intentelo de nuevo.")
-		self.listaVect=np.array(self.listaCoef)
+		self.listaVect=np.array(self.listaCoef)				# conversión de la lista en vector para procesos matemáticos
 		print("Polinomio ingresado: ")
 		mostrarPolin(self.listaVect)			
 
@@ -43,19 +43,19 @@ def multipliEscalar(poli):
 			escalar=float(input("Ingrese el escalar por el que desea multiplicar: "))
 			break;
 		except ValueError:
-			print("Valores no válidos. Recuerde que debe ingresar un valor numerico valido.")
+			print("Valores no válidos. Recuerde que debe ingresar un valor numérico valido.")
 
-	print("El resultado de la multiplicación escalar es :",poli.listaVect*escalar)
+	print("El resultado de la multiplicación escalar es :",poli.listaVect*escalar)  # cálculo e impresión de polinomio resultante
 	mostrarPolin(poli.listaVect*escalar)
 	return poli.listaVect*escalar
 
 
 def sumaPolin(poli1,poli2):
 	
-	difOrden=(len(poli1.listaVect)-len(poli2.listaVect))
-	vectCeros=np.zeros(abs(difOrden))
+	difOrden=(len(poli1.listaVect)-len(poli2.listaVect))   # determina el orden de los polinomios para proceder con la 
+	vectCeros=np.zeros(abs(difOrden))					   # igualación de términos
 		
-	if len(poli1.listaVect)>len(poli2.listaVect):
+	if len(poli1.listaVect)>len(poli2.listaVect):      	   # las siguientes condiciones permiten completar el vector más pequeño
 
 		print("El resultado de la suma de los polinomios es :",poli1.listaVect+np.array(vectCeros.tolist()+poli2.listaCoef))
 		mostrarPolin(poli1.listaVect+np.array(vectCeros.tolist()+poli2.listaCoef))
@@ -68,17 +68,17 @@ def sumaPolin(poli1,poli2):
 		return poli2.listaVect+np.array(vectCeros.tolist()+poli1.listaCoef)
 	else:
 
-		print("El resultado de la suma de los polinomios es :")
+		print("El resultado de la suma de los polinomios es :")    # esta caso para cuando las longitudes de los vectores son iguales
 		mostrarPolin(poli2.listaVect+poli1.listaVect)
 		return poli2.listaVect+poli1.listaVect
 
 
 def restaPolin(poli1,poli2):
 	
-	difOrden=(len(poli1.listaVect)-len(poli2.listaVect))
-	vectCeros=np.zeros(abs(difOrden))
+	difOrden=(len(poli1.listaVect)-len(poli2.listaVect))	# determina el orden de los polinomios para proceder con la 
+	vectCeros=np.zeros(abs(difOrden))						# igualación de términos
 		
-	if len(poli1.listaVect)>len(poli2.listaVect):
+	if len(poli1.listaVect)>len(poli2.listaVect):			# condiciones para determinar cual vector se debe completar con ceros
 
 		print("El resultado de la resta de los polinomios es :")
 		mostrarPolin(poli1.listaVect-np.array(vectCeros.tolist()+poli2.listaCoef))
@@ -92,19 +92,19 @@ def restaPolin(poli1,poli2):
 
 	else:
 
-		print("El resultado de la resta de los polinomios es :")
+		print("El resultado de la resta de los polinomios es :")  # caso para cuando sus longitudes coinciden (caso feliz)
 		mostrarPolin(poli1.listaVect-poli2.listaVect)
 		return poli1.listaVect-poli2.listaVect
 
 
 def multipliPolin(poli1,poli2):
 
-	vectResult=np.zeros((len(poli1.listaVect)+len(poli2.listaVect))-1)
-
-	for i in range (len(poli1.listaVect)):
-
-		for j in range(len(poli2.listaVect)):
-				vectResult[i+j]=vectResult[i+j]+(poli1.listaVect[i]*poli2.listaVect[j])
+	vectResult=np.zeros((len(poli1.listaVect)+len(poli2.listaVect))-1)  # tamaño del vector resultante según el tamaño de 
+																		# los vectores de los polinomios asociados a la operación
+	for i in range (len(poli1.listaVect)):								
+																		# se recorren los vectores asemejandose a una distributiva
+		for j in range(len(poli2.listaVect)):							
+				vectResult[i+j]=vectResult[i+j]+(poli1.listaVect[i]*poli2.listaVect[j])  # vector con las multiplicaciones y sumas resultantes
 
 	print("El polinomio resultante luego de multiplicarlos es: ", "\n")
 	mostrarPolin(vectResult)			
@@ -116,15 +116,15 @@ def evaluarPolin(poliResult):
 
 	potencia=len(poliResult)
 	resultado=0
-	while True:
+	while True:				# toma el valor para evaluar en el polinomio que se ingrese en la función
 	    try:
-	    	x=float(input("Por favor ingrese el valor para reemplazar en X: "))	
+	    	x=float(input("Por favor ingrese el valor para evaluar en X: "))		# validación de datos ingresados
 	    	break;
 	    except ValueError:
 	    	print ("Valor inválido. Por favor inténtelo de nuevo.")
 
 	for i in range(len(poliResult)):
-		resultado=resultado+(poliResult[i]*(x**(len(poliResult)-1-i)))
+		resultado=resultado+(poliResult[i]*(x**(len(poliResult)-1-i)))	 # acumulador para resultado de evaluación y suma de términos
 
 	  
 	print("Al evaluar el polinomio resulta:",resultado)
@@ -132,13 +132,25 @@ def evaluarPolin(poliResult):
 
 def mostrarPolin(poli):
 	
-	print(f" ({poli[0]})X^{len(poli)-1}", end="")
+	print(f" ({poli[0]})X^{len(poli)-1}", end="")		        # impresión del primer término
 	for i in range(len(poli)):
 		if i!=(len(poli)-1) and i!=0:
-			print(f" + ({poli[i]})X^{len(poli)-1-i}", end="")
-	print(f" + ({poli[i]})X^{len(poli)-1-i}","\n") 
+			print(f" + ({poli[i]})X^{len(poli)-1-i}", end="")	# impresión de tértminos en medio	
+	print(f" + ({poli[i]})X^{len(poli)-1-i}","\n") 		        # impresión el último termino
 
-	
+def validacion(vector):
+	while True:
+			try:
+				opcion=int(input("¿Desea evaluar el polinomio previo? 1.SI  2.NO (ingrese el valor numérico asociado a la opción): "))
+				if opcion==1:
+					evaluarPolin(vector)
+					break;
+				elif opcion==2:
+					break;
+				else:
+					print("Opción inexistente. Por favor ingresa un valor asociado en el menú.")
+			except ValueError:
+				print("Valor de opción incorrecto. Por favor, inténtalo de nuevo.")
 
 
 ################################# Main ###########################################
@@ -148,8 +160,8 @@ while True:
 
 		try:
 			print("#################################","\n",
-			"# 1.sumar                         #","\n",
-			"# 2.restar                        #","\n",
+			"# 1.sumar polinomios              #","\n",
+			"# 2.restar polinomios             #","\n",
 			"# 3.multiplicar por escalar       #","\n",
 			"# 4.multiplicación de polinomios  #","\n",
 			"# 5.evaluar polinomio             #","\n",
@@ -164,25 +176,27 @@ while True:
 		polinomio1=polinomio()
 		polinomio2=polinomio()
 		vector=sumaPolin(polinomio1,polinomio2)
-		evaluarPolin(vector)
+		validacion(vector)		
 	elif opcion==2:
+		print("IMPORTANTE: ingrese primero el polinomio 'minuendo', posteriormente ingrese el polinomio 'sustraendo'", "\n")
 		polinomio1=polinomio()
 		polinomio2=polinomio()
 		vector=restaPolin(polinomio1,polinomio2)
-		evaluarPolin(vector)
+		validacion(vector)
 	elif opcion==3:
 		polinomio1=polinomio()
 		vector=multipliEscalar(polinomio1)
-		evaluarPolin(vector)
+		validacion(vector)
 	elif opcion==4:
 		polinomio1=polinomio()
 		polinomio2=polinomio()	
 		vector=multipliPolin(polinomio1,polinomio2)
-		evaluarPolin(vector)
+		validacion(vector)
 	elif opcion==5:
 		polinomio1=polinomio()
 		evaluarPolin(polinomio1.listaVect)
 	elif opcion==6:
+		print("Hasta pronto. Programa finalizado")
 		break;
 	else:
 		print("Opción inexistente. Por favor ingresa un valor asociado en el menú.")
